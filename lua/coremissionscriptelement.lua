d7a0158031ae293eab457864b000311f3d84572f
Dr_Newbie <tf2baidonation@gmail.com>
2016-08-11 23:19:15 +0800

    3 ; Faster Bile
    https://youtu.be/1I5VqGOiy_M

--- lua/coremissionscriptelement.lua
+++ lua/coremissionscriptelement.lua
@@ -5,7 +5,11 @@ core:import("CoreClass")
 
 local CookFaster = _G.CookFaster or {}
 
-if not CookFaster then
+if not CookFaster or not CookFaster.settings then
+	return
+end
+
+if not Global.game_settings or (Global.game_settings.level_id ~= "rat" and Global.game_settings.level_id ~= "alex_1") then
 	return
 end
 
@@ -13,14 +17,14 @@ local _f_MissionScriptElement_on_executed = MissionScriptElement.on_executed
 
 function MissionScriptElement:on_executed(instigator, alternative, skip_execute_on_executed)
 	local _id = tostring(self._id)
-	if Global.game_settings then
-		if Global.game_settings.level_id == "rat" or Global.game_settings.level_id == "alex_1" then
-			if _id == "100724" then
-				local element = self:get_mission_element(100494)
-				CookFaster:DelayScript(self, element, instigator)
-				return
-			end
-		end
+	if _id == "100724" then
+		local element = self:get_mission_element(100494)
+		CookFaster:DelayScript(self, element, instigator, CookFaster.settings.Delay)
+		return
+	end
+	if _id == "102197" and CookFaster.settings.Bile_Coming then	
+		local element = self:get_mission_element(102263)
+		CookFaster:DelayScript(self, element, instigator, 1)
 	end
 	_f_MissionScriptElement_on_executed(self, instigator, alternative, skip_execute_on_executed)
 end
--- lua/function4others.lua
+++ lua/function4others.lua
@@ -8,6 +8,7 @@ CookFaster.settings = CookFaster.settings or {}
 function CookFaster:Reset()
 	self.settings = {
 		Delay = 1,
+		Bile_Coming = true,
 	}
 	self:Save()
 end
@@ -38,8 +39,10 @@ Hooks:Add("LocalizationManagerPostInit", "CookFaster_loc", function(loc)
 	LocalizationManager:add_localized_strings({
 		["CookFaster_menu_title"] = "Cook Faster",
 		["CookFaster_menu_desc"] = "",
-		["CookFaster_menu_delay_title"] = "Time for Next Thing",
-		["CookFaster_menu_delay_desc"] = "",
+		["CookFaster_menu_Delay_title"] = "Time for Next Thing",
+		["CookFaster_menu_Delay_desc"] = "",
+		["CookFaster_menu_Bile_Coming_title"] = "Faster Bile",
+		["CookFaster_menu_Bile_Coming_desc"] = "",
 	})
 end)
 
@@ -48,14 +51,14 @@ Hooks:Add("MenuManagerSetupCustomMenus", "CookFasterOptions", function( menu_man
 end)
 
 Hooks:Add("MenuManagerPopulateCustomMenus", "CookFasterOptions", function( menu_manager, nodes )
-	MenuCallbackHandler.CookFaster_menu_delay_callback = function(self, item)
+	MenuCallbackHandler.CookFaster_menu_Delay_callback = function(self, item)
 		CookFaster.settings.Delay = math.floor(item:value())
 		CookFaster:Save()
 	end
 	MenuHelper:AddSlider({
-		id = "CookFaster_menu_delay_callback",
-		title = "CookFaster_menu_delay_title",
-		callback = "CookFaster_menu_delay_callback",
+		id = "CookFaster_menu_Delay_callback",
+		title = "CookFaster_menu_Delay_title",
+		callback = "CookFaster_menu_Delay_callback",
 		value = CookFaster.settings.Delay,
 		min = 1,
 		max = 45,
@@ -63,15 +66,29 @@ Hooks:Add("MenuManagerPopulateCustomMenus", "CookFasterOptions", function( menu_
 		show_value = true,
 		menu_id = CookFaster.options_menu,  
 	})
+	MenuCallbackHandler.CookFaster_menu_Bile_Coming_callback = function(self, item)
+		CookFaster.settings.Bile_Coming = item:value() == "on" and true or false
+		CookFaster:Save()
+	end
+	MenuHelper:AddToggle({
+		id = "CookFaster_menu_Bile_Coming_callback",
+		title = "CookFaster_menu_Bile_Coming_title",
+		callback = "CookFaster_menu_Bile_Coming_callback",
+		value = CookFaster.settings.Bile_Coming,
+		menu_id = CookFaster.options_menu,  
+	})
 end)
 
 Hooks:Add("MenuManagerBuildCustomMenus", "CookFasterOptions", function(menu_manager, nodes)
 	nodes[CookFaster.options_menu] = MenuHelper:BuildMenu( CookFaster.options_menu )
-	MenuHelper:AddMenuItem( MenuHelper.menus.lua_mod_options_menu, CookFaster.options_menu, "CookFaster_menu_title", "CookFaster_menu_desc", 1 )
+	MenuHelper:AddMenuItem( MenuHelper.menus.lua_mod_options_menu, CookFaster.options_menu, "CookFaster_menu_title", "CookFaster_menu_desc")
 end)
 
-function CookFaster:DelayScript(them, element, instigator)
-	DelayedCalls:Add("DelayedCalls_CookFaster_DelayScript", CookFaster.settings.Delay, function()
+function CookFaster:DelayScript(them, element, instigator, delay)
+	if not delay or delay < 0 then
+		delay = 1
+	end
+	DelayedCalls:Add("DelayedCalls_CookFaster_DelayScript", delay, function()
 		if them and element and instigator then
 			them._mission_script:add(callback(element, element, "on_executed", instigator), 0.1, 1)
 			managers.network:session():send_to_peers_synched("run_mission_element", 100494, instigator, 0)
