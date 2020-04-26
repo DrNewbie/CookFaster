4dc427d9519d32ea6d30f93cea7026f6205353e3
Dr_Newbie <tf2baidonation@gmail.com>
2020-04-27 01:09:34 +0800

    12 ; Cook Off (Daylight)
    https://modworkshop.net/mod/27373

--- CookFaster/lua/coreelementtimer.lua
+++ CookFaster/lua/coreelementtimer.lua
@@ -8,13 +8,17 @@ if not CookFaster or not CookFaster.settings then
 	return
 end
 
-if not Global.game_settings or (Global.game_settings.level_id ~= "rat" and Global.game_settings.level_id ~= "alex_1") then
+if not Global.game_settings or (Global.game_settings.level_id ~= "rat" and Global.game_settings.level_id ~= "alex_1" and Global.game_settings.level_id ~= "ratdaylight") then
 	return
 end
 
 Hooks:PostHook(ElementTimer, "timer_operation_reset", "CookFaster_timer_operation_reset", function(self)
-	if (self._id == 102164 or self._id == 102166 or self._id == 102177 or self._id == 102178) and CookFaster.settings.Bile_Coming then
-		self._timer = self:get_random_table_value_float(1)
-		self:_update_digital_guis_timer()
+	if not Global.game_settings or (Global.game_settings.level_id ~= "rat" and Global.game_settings.level_id ~= "alex_1" and Global.game_settings.level_id ~= "ratdaylight") then
+	
+	else
+		if (self._id == 102164 or self._id == 102166 or self._id == 102177 or self._id == 102178) and CookFaster.settings.Bile_Coming then
+			self._timer = self:get_random_table_value_float(1)
+			self:_update_digital_guis_timer()
+		end
 	end
 end)
--- CookFaster/lua/coremissionscriptelement.lua
+++ CookFaster/lua/coremissionscriptelement.lua
@@ -10,12 +10,13 @@ end
 Hooks:PreHook(MissionScriptElement, "_calc_base_delay", "CookFaster_calc_base_delay", function(self)
 	local CookFaster = _G.CookFaster or {}
 	if not CookFaster or not CookFaster.settings then
-		return
-	end
-	if Global.game_settings.level_id == "rat" or Global.game_settings.level_id == "alex_1" then
-		if self._id == 102197 and CookFaster.settings.Bile_Coming then
-			self._values.base_delay_rand = nil
-			self._values.base_delay = 1
+	
+	else
+		if Global.game_settings.level_id == "rat" or Global.game_settings.level_id == "alex_1" or Global.game_settings.level_id == "ratdaylight" then
+			if self._id == 102197 and CookFaster.settings.Bile_Coming then
+				self._values.base_delay_rand = nil
+				self._values.base_delay = 1
+			end
 		end
 	end
 end)
@@ -23,13 +24,14 @@ end)
 Hooks:PreHook(MissionScriptElement, "_calc_element_delay", "CookFaster_calc_element_delay", function(self, params)
 	local CookFaster = _G.CookFaster or {}
 	if not CookFaster or not CookFaster.settings then
-		return
-	end
-	if ((Global.game_settings.level_id == "rat" or Global.game_settings.level_id == "alex_1") and self._id == 100724) or 
-		(Global.game_settings.level_id == "mex_cooking" and self._editor_name == "counter_below3") or 
-		((Global.game_settings.level_id == "mia_1" or Global.game_settings.level_id == "crojob2") and self._editor_name == "timer_to_next") then
-		params.delay_rand = nil
-		params.delay = CookFaster.settings.Delay
-		return CookFaster.settings.Delay + 0.1
+	
+	else
+		if ((Global.game_settings.level_id == "rat" or Global.game_settings.level_id == "alex_1" or Global.game_settings.level_id == "ratdaylight") and self._id == 100724) or 
+			(Global.game_settings.level_id == "mex_cooking" and self._editor_name == "counter_below3") or 
+			((Global.game_settings.level_id == "mia_1" or Global.game_settings.level_id == "crojob2") and self._editor_name == "timer_to_next") then
+			params.delay_rand = nil
+			params.delay = CookFaster.settings.Delay
+			return CookFaster.settings.Delay + 0.1
+		end
 	end
 end)
